d8b97d78019e08b6ada8979ba3744bdc807b4707
jeongha-joo <jha970107@gmail.com>
2022-09-15 15:44:44 +0900

    build

--- config/config.ts
+++ config/config.ts
@@ -2,13 +2,25 @@ import { readFileSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { join } from 'path';
 
-const YAML_CONFIG_PROD = 'production.yaml';
-const YAML_CONFIG_DEV = 'development.yaml';
+// const YAML_CONFIG_PROD = 'production.yaml';
+// const YAML_CONFIG_DEV = 'development.yaml';
 
 export default () => {
-  return yaml.load(
-    process.env.NODE_ENV === 'production'
-      ? readFileSync(join(__dirname, YAML_CONFIG_PROD), 'utf8')
-      : readFileSync(join(__dirname, YAML_CONFIG_DEV), 'utf8'),
-  ) as Record<string, any>;
+  return {
+    server: {
+      port: process.env.PORT,
+    },
+
+    db: {
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      synchronize: true,
+    },
+
+    jwt: {
+      expiresIn: 3600,
+      secret: process.env.JWT_SECRET,
+    },
+  } as Record<string, any>;
 };
