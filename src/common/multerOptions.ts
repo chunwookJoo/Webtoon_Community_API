24b08a8982af728076e051e94bc6db3ab60ae20b
jeongha-joo <jha970107@gmail.com>
2022-10-18 18:38:02 +0900

    build

--- src/common/multerOptions.ts
+++ src/common/multerOptions.ts
@@ -32,8 +32,8 @@ export const multerOptions = {
 };
 
 export const createImageURL = (file): string => {
-  const serverAddress: string = process.env.SERVER_ADDRESS;
+  // const serverAddress: string = process.env.SERVER_ADDRESS;
   // 파일이 저장되는 경로: 서버주소/public 폴더
   // 위의 조건에 따라 파일의 경로를 생성해줍니다.
-  return `${serverAddress}/userProfileImages/${file.filename}`;
+  return `/storage/userProfileImages/${file.filename}`;
 };
